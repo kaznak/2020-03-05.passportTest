46f1dc14d195108e3dbe80b02a850c6c5cae08fc
kaznak <kaznak.at.work@gmail.com>
2020-03-05 20:36:52 +0900

    add auth features

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require("express");
 var router = express.Router();
 
+var passport = require("passport");
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", user: req.user });
+});
+
+router.get("/login", function(req, res) {
+  res.render("login", { user: req.user });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    session: true,
+  })
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    session: false,
+  })
+);
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
